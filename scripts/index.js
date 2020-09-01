cb34ae28079ec33756604a30b6d384f5bb4acbf6
Iurii Bairamukov <juribai@mail.ru>
2020-09-02 06:00:54 +0700

    feat: привязать смену цвета к API

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,8 @@
-const button = document.querySelector('.button')
-const colorList = ['#BCADD6', '#FFCF01', '#F37054', '#49B9BB', '#775BA6',
-'#4CBD9F', '#F15C72', '#F48B90', '#7D8A3A', '#0082CE']
+const button = document.querySelector('.button');
+const color = document.querySelector('.color');
 
-const color = document.querySelector('.color')
-
-const changeColor = function () {
-  const indexColor = Math.floor(Math.random() * colorList.length);
-  color.style.backgroundColor = colorList[indexColor];
-}
-
-button.addEventListener('click', changeColor)
+button.addEventListener('click', () => {
+  fetch('http://www.colr.org/json/color/random')
+    .then(res => res.json())
+    .then(data => color.style.backgroundColor = `#${data.new_color}`);
+})
