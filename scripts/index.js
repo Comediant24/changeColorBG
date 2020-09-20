1880ad24304027e5ca9dbf4d352efed55e11d898
Iurii Bairamukov <juribai@mail.ru>
2020-09-20 19:34:35 +0700

    fix: обращение к CORS API

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,9 @@ const button = document.querySelector('.button');
 const color = document.querySelector('.color');
 
 button.addEventListener('click', () => {
-  fetch('http://www.colr.org/json/color/random')
-    .then(res => res.json())
-    .then(data => color.style.backgroundColor = `#${data.new_color}`);
-})
+  fetch(
+    'https://cors-anywhere.herokuapp.com/http://www.colr.org/json/color/random'
+  )
+    .then((res) => res.json())
+    .then((data) => (color.style.backgroundColor = `#${data.new_color}`));
+});
